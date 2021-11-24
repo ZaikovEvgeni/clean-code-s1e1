9397bcb0795720ceee25cf885c22509e539870b0
Zaikov Evgeni <evgeniikas9@gmail.com>
2021-11-24 17:30:34 +0300

    refactor: HTML,CSS,JS formating selectors BEM metodology  adjust according to rule 3.1,3.2,3.3  in html-and-css.md

--- app.js
+++ app.js
@@ -42,10 +42,11 @@ var createNewTaskElement=function(taskString){
     editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="tasks__item-edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="tasks__item-delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = 'tasks__item-delete-img';
     deleteButton.appendChild(deleteButtonImg);
 
 
@@ -85,7 +86,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".tasks__item-edit");
     var containsClass=listItem.classList.contains("editMode");
     //If class of the parent is .editmode
     if(containsClass){
@@ -157,8 +158,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector(".tasks__item-edit");
+    var deleteButton=taskListItem.querySelector(".tasks__item-delete");
 
 
     //Bind editTask to edit button.
